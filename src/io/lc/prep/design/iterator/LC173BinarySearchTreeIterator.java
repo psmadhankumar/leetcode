495cd4973c602ae5b39a02d474343d1bb8a72d75
psmadhankumar <psmadhankumar@gmail.com>
2019-12-31 01:07:08 -0800

    LC173

---
+++ src/io/lc/prep/design/iterator/LC173BinarySearchTreeIterator.java
@@ -0,0 +1,67 @@
+package io.lc.prep.design.iterator;
+
+import io.lc.prep.tree.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+ *
+ * Calling next() will return the next smallest number in the BST.
+ *
+ * Example:
+ *
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ *
+ *
+ * Note:
+ *
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
+ *
+ *
+ *
+ */
+public class LC173BinarySearchTreeIterator {
+    Stack<TreeNode> stack = null;
+    
+    public LC173BinarySearchTreeIterator(TreeNode root) {
+        stack = new Stack<>();
+
+        while(root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if(node.right != null) {
+            TreeNode current = node.right;
+
+            while(current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+        }
+
+        return node.val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+}
